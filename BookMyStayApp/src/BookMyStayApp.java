3574356b7cbcb43dcd3e0055165f9212c9db82e4
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 10:02:55 +0530

    UC2: Room modeling with abstract classes

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,14 +1,41 @@
-/**
- * Book My Stay App
- * Demonstrates application start and console output
- * @author Chaitanya
- * @version 1.0
- */
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public abstract void displayDetails();
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() { super("Single", 1, 100); }
+    public void displayDetails() { System.out.println(type + " Room: Beds=" + beds + ", Price=" + price); }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() { super("Double", 2, 180); }
+    public void displayDetails() { System.out.println(type + " Room: Beds=" + beds + ", Price=" + price); }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() { super("Suite", 3, 300); }
+    public void displayDetails() { System.out.println(type + " Room: Beds=" + beds + ", Price=" + price); }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay App!");
-        System.out.println("Application Version: 1.0");
-    }
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
+        single.displayDetails();
+        doubleRoom.displayDetails();
+        suite.displayDetails();
+    }
 }
