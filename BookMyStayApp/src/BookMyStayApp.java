75ce455beec3fd2593f5f364e05cd20c5d1be691
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 10:02:14 +0530

    UC1: Welcome message

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,2 +1,14 @@
+/**
+ * Book My Stay App
+ * Demonstrates application start and console output
+ * @author Chaitanya
+ * @version 1.0
+ */
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Application Version: 1.0");
+    }
+
 }
