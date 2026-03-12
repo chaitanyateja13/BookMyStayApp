4821bacf3c781061d68264b9dc87134c634bea3a
chaitanya teja <cg5501@srmist.edu.in>
2026-03-12 10:42:49 +0530

    BookMyStayApp.java

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,9 +1,4 @@
-/**
- * Book My Stay App
- * Demonstrates application start and console output
- * @author Chaitanya
- * @version 1.0
- */
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
