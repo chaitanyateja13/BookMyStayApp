aa96c108ca85c9a70e71855fd8bd5d52d05ba46f
chaitanyateja13 <vinaychauhan5380@gmail.com>
2026-03-12 08:56:52 +0530

    UC6: Safe room allocation using Set and Map

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,2 +1,51 @@
+import java.util.*;
+
 public class BookMyStayApp {
+
+    static Queue<String> bookingQueue = new LinkedList<>();
+
+    static Map<String, Integer> inventory = new HashMap<>();
+
+    static Map<String, Set<String>> allocatedRooms = new HashMap<>();
+
+    static int roomCounter = 1;
+
+    public static void main(String[] args) {
+
+        inventory.put("DELUXE", 2);
+        inventory.put("STANDARD", 3);
+
+        bookingQueue.add("DELUXE");
+        bookingQueue.add("STANDARD");
+        bookingQueue.add("DELUXE");
+
+        processBookings();
+    }
+
+    public static void processBookings() {
+
+        while(!bookingQueue.isEmpty()) {
+
+            String roomType = bookingQueue.poll();
+
+            int available = inventory.getOrDefault(roomType,0);
+
+            if(available > 0) {
+
+                String roomId = "ROOM-" + roomCounter++;
+
+                allocatedRooms
+                        .computeIfAbsent(roomType,k->new HashSet<>())
+                        .add(roomId);
+
+                inventory.put(roomType,available-1);
+
+                System.out.println("Reservation confirmed : "+roomType+" -> "+roomId);
+
+            } else {
+
+                System.out.println("No rooms available for "+roomType);
+            }
+        }
+    }
 }
